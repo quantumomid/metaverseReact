1ad742fe74d99760ede6289f256ab8afe2812cee
quantumomid <omid.wakili@outlook.com>
2021-12-16 11:17:09 +0000

    first commit adding next js files with home page and login components

---
+++ components/Login.js
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import { useMoralis } from "react-moralis";
+
+const Login = () => {
+    const { authenticate } = useMoralis();
+    return (
+        <div className="bg-black relative text-white">
+            <h2>I am a Login screen</h2>
+            <div className="flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4">
+                <Image
+                    className="object-cover rounded-full" 
+                    src="/loginImage.jpg"
+                    width={200}
+                    height={200}
+                />
+                <button onClick={authenticate} className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse">Login to the METAVERSE</button>
+            </div>
+            <div className="w-full h-screen">
+                <Image 
+                    src="https://links.papareact.com/55n" 
+                    layout="fill"
+                    objectFit="cover"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Login;

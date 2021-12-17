961695dd36a6bf3cbcdc66c798011268d4fc3afa
quantumomid <omid.wakili@outlook.com>
2021-12-17 12:04:30 +0000

    start building message component to show user messages in the chat

--- components/Message.js
+++ components/Message.js
@@ -2,10 +2,21 @@ import { useMoralis } from "react-moralis";
 
 const Message = ({ message }) => {
     const { user } = useMoralis();
-    
+
+    const isUserMessage = message.get("ethAddress") === user.get("ethAddress");
+
     return (
-        <div>
-            { message.attributes.message }
+        <div className={`flex items-end space-x-2 relative ${
+            isUserMessage && "justify-end"
+            }`}
+        >
+            <div className={`flex space-x-4 p-3 rounded-lg ${
+                isUserMessage ? "rounded-br-none bg-pink-300"
+                    : "rounded-bl-none bg-blue-400"
+                }`}
+            >
+                <p>{ message.get("message") }</p>
+            </div>
         </div>
     )
 }
